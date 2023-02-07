815701f5c06b2ceeb5c3c38f19df4dd550c53f1c
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-06 18:56:51 -0600

    updated rock paper scissors with while loop to continue game play

--- rock-paper-scissors/rockpaper.js
+++ rock-paper-scissors/rockpaper.js
@@ -1,17 +1,17 @@
-//playing a game - Rock Paper Scissors
-//Human vs Computer
+// Playing a game - ROCK PAPER SCISSORS
+// Human vs Computer
 
-//Human choice is set in code
+// Human choice is set in code
 let humanInput = "scissors";
 
 let humanChoice;
 let computerChoice;
 
+let continueGame = true; //flag
+
 humanInput = humanInput.toUpperCase();
 
 // convert human choice string to a number
-
-
 if (humanInput == "ROCK") {
     humanChoice = 1;
 }
@@ -22,27 +22,49 @@ if (humanInput == "PAPER") {
 
 if (humanInput == "SCISSORS") {
     humanChoice = 3;
-
-}
-computerChoice = getRandomNumber(1,3);
-console.log(`computer choice ${computerChoice}`);
-console.log(`human choice ${humanChoice}`);
-
-if (humanChoice == computerChoice) {
-    console.log("The game is a draw");
 }
 
-if (humanChoice > computerChoice) {
-    console.log("Human wins!!!")
-}
+while (continueGame == true) {
 
-if (computerChoice > humanChoice) {
-    console.log("Computer wins!!!")
-}
+    computerChoice = getRandomNumber(1, 3);
 
-    
-    function getRandomNumber(min, max){
-        let randomNumber;
-        randomNumber = Math.floor(Math.random() * (max - min + 1 ) + min);
-        return randomNumber;
+    console.log(`human choice ${humanChoice}`)
+    console.log(`computer choice ${computerChoice}`)
+
+    if (humanChoice == computerChoice) {
+        console.log("The game is a DRAW");
     }
+
+    // Human chooses ROCK (1) and Computer chooses SCISSORS (3), HUMAN WINS
+    if (humanChoice == 1 && computerChoice == 3) {
+        console.log("Human wins !!!");
+        continueGame = false;
+    } else if (humanChoice > computerChoice) {
+        console.log("Human wins !!!");
+        continueGame = false;
+    } else if (computerChoice > humanChoice) {
+        console.log("Computer wins :( ");
+        continueGame = false;
+    }
+
+}
+
+// ======= FUNCTIONS ARE BELOW ======== //
+
+function getRandomNumber(min, max) {
+    let randomNumber;
+    randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
+}
+
+
+// Computer will respond with a choice
+
+// Apply rules: Who wins
+
+// Need to ensure the values are case insensitive "ROCK" "rock" "rOCk"
+// Compare values using IF
+
+// Important: If there is a TIE, please continue the game
+
+// Display the winner
