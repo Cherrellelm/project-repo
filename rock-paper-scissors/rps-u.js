4653c108171d0d2cb71a10cf9ccf1c4f9bb8d37c
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-12 18:33:34 -0500

    created two functions getComputerChoice and PlayRound to play the game

---
+++ rock-paper-scissors/rps-u.js
@@ -0,0 +1,21 @@
+//Let's play rock, paper, scissors
+let playOptions = ["rock", "paper", "scissors"];
+let randomOption = playOptions[Math.floor(Math.random() * playOptions.length)];
+
+function getComputerChoice () {
+    return randomOption;
+    
+}
+console.log(randomOption);
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    console.log(playRound(playerSelection, computerSelection));
+  }
+   
+
+
