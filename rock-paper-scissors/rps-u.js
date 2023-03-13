ed7cea2a9326b9ed75a85f719f02a23e6b22a8b6
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-12 21:18:57 -0500

    added notes to complete code for game

--- rock-paper-scissors/rps-u.js
+++ rock-paper-scissors/rps-u.js
@@ -4,10 +4,10 @@ let randomOption = playOptions[Math.floor(Math.random() * playOptions.length)];
 
 function getComputerChoice () {
     return randomOption;
-    
 }
 
 let playerInput = prompt("Make your move!");
+let playerWins = "win";
 let playerSelection = playerInput;
 let computerSelection = getComputerChoice();
 
@@ -18,30 +18,32 @@ function playRound(playerSelection, computerSelection) {
     console.log("Player Selection is " + playerSelection);
     console.log("Computer Selection is " + randomOption);
     if (playerSelection == "rock" && computerSelection == "rock") {
-        console.log("It's a draw, everybody wins!");
+        console.log("It's a draw!");
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         console.log("You lose, Paper covers Rock!");
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win, Rock beats Scissors!");
+        console.log(`You ${playerWins}, Rock beats Scissors!`);
     }
 
     if (playerSelection == "paper" && computerSelection == "paper") {
-        console.log("It's a draw, everybody wins!");
+        console.log("It's a draw!");
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win, Paper covers Rock!");
+        console.log(`You ${playerWins}, Paper covers Rock!`);
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         console.log("You lose, Scissors cuts Paper!");
     }
 
     if (playerSelection == "scissors" && computerSelection == "scissors") {
-        console.log("It's a draw, everybody wins!");
+        console.log("It's a draw!");
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         console.log("You lose, Rock beats Scissors!");
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win, Scissors cuts Paper!");
+        console.log(`You ${playerWins}, Scissors cuts Paper!`);
     }
   }
   
+ let playerPoint = 1;
+ let computerPoint = 1;
 
   function game (){
     for (let counter = 0; counter < 5; counter++) {
@@ -49,11 +51,21 @@ function playRound(playerSelection, computerSelection) {
         if (counter => 1) {
         console.log("Round " + (counter +1))
         }
-        playRound(playerSelection, computerSelection);
+
+        playRound(playerSelection, randomOption);
      }
-     
+
+     if (playerWins.includes("win")){
+        console.log("Player gets " + playerPoint + "point!" );
+    }
+    // else {
+        //console.log("Computer gets " + computerPoint + "point!")
+    //}
   }
   
 game();
+
+//Need to figure out. howto generate random number each round
+//need to add total points each time a player wins around
 
 
