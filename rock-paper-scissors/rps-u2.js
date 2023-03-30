845d1ce77b3c93b9376cb236d20e8fd88fd4c3cc
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-29 19:59:02 -0500

    replace value for Make a selection with placeholder

--- rock-paper-scissors/rps-u2.js
+++ rock-paper-scissors/rps-u2.js
@@ -28,13 +28,11 @@ let playerInputBox = () => {
   playDiv.setAttribute("id", "playDiv");
   playerText.setAttribute("id", playerTextId);
   playerText.setAttribute("type", "text");
-  playerText.setAttribute("value", "Make a selection");
+  playerText.setAttribute("placeholder", "Make a selection");
   gameSheet.appendChild(playDiv);
   playDiv.appendChild(playerText);
   //playDiv.appendChild(gameBorderId);
   document.getElementById("playerText").style.margin = "7px 30px 20px 30px";
-
-  playerText.addEventListener("click", () => clearInput());
 
   let startBtnDiv = document.createElement("div");
   let startBtnId = "startBtn";
@@ -57,10 +55,6 @@ let playerInputBox = () => {
   outcomeText = document.createElement("p");
   outcomeText.textContent = score;*/
 };
-
-function clearInput() {
-  return (playerText.value = "");
-}
 
 let compBox = () => {
   let compDiv = document.createElement("div");
--- rock-paper-scissors/test.js
+++ rock-paper-scissors/test.js
@@ -1,18 +1,22 @@
 let playerText = document.getElementById("playerText");
 let goBtn = document.getElementById("goBtn");
 let playDiv = document.getElementById("playDiv");
+let playerSelection = playerText.value;
 
-playerText.addEventListener("click", () => clearInput());
+//playerText.addEventListener("click", () => clearInput());
 playerText.addEventListener("textInput", () => textBox());
+goBtn.addEventListener("click", () => clickBtn());
 
 function clearInput() {
   return (playerText.value = "");
 }
 
 function textBox() {
-  console.log("text entered");
+  playerText.value = "";
+  console.log(playerSelection);
 }
 
-function clickBtn(btn) {
-  console.log("clicked");
+function clickBtn() {
+  return playerSelection;
+  console.log();
 }
