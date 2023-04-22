c4bd44ac67d19129f349c629c29cee3d59306b55
Cherrellelm <cherrelle.l.m@gmail.com>
2023-04-21 23:17:57 -0500

    created getDisplayNumber function

--- Calculator+/script.js
+++ Calculator+/script.js
@@ -13,28 +13,63 @@ clear() {
 }
 
 delete() {
-
+this.currentOperand = this.currentOperand.toString().slice(0, -1);
 }
 
 appendNumber(number) {
-    if(number == '.' && this.currentOperand.includes('.')) return;
+    if(number === '.' && this.currentOperand.includes('.')) return;
     this.currentOperand = this.currentOperand.toString() + number.toString();
 }
 
 chooseOperation(operation) {
-
+    if(this.currentOperand === '') return;
+    if (this.previousOperand !== '') {
+        this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
 }
 
 compute(){
-
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+        case '+':
+            computation = prev + current
+            break;
+        case '-':
+            computation = prev - current
+            break
+        case '*':
+            computation = prev * current
+             break;
+        case '÷':
+            computation = prev / current
+            break;
+        default:
+            return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
+}
+gitDisplayNumber(number) {
+    return number 
 }
 
 updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.currentOperandTextElement.innerText = this.gitDisplayNumber (this.currentOperand);
+    if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+        `${this.gitDisplayNumber(this.previousOperand)} ${this.operation}`
+    
+    }
+    //this.previousOperandTextElement.innerText = this.previousOperand;
 }
 }
-
-
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -58,4 +93,19 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
         calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     })
+ })
+
+ equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+ })
+
+ allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+ })
+
+ deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
  })
